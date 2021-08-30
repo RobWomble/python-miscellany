47b5ce549193fbe139be6c00512dc302c3c3a59d
Rob Womble <RobWomble@users.noreply.github.com>
2021-08-30 03:08:40 -0700

    learned zip() and itertools

--- digimon_tag_script.py
+++ digimon_tag_script.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python3
-""" Tag script | by Rob Womble
-    script to determine ideal button
-    sequence for consistent results
-    in tag training on
-    Digimon 20th anniversary vpet"""
+""" digimon tag script
+    by Rob Womble       """
 
-# not sure if I need the whole module or just one function
-import itertools
+from itertools import product
 
+''' The purpose of this script was to solve an issue I had been
+    wracking my mind over.
 
-# tuple of tuples containing the sequences to beat
+    I had recently dug out my Digimon 20th Anniversary V-Pet
+    with the intention of starting a new set of mons.  For those
+    unaware, digmon v-pet toys are similar to the tomagotchi toys of
+    olden days in that one cares for a virtual creature. The difference
+    in digimon is that these creatures are also trained to do battle
+    with each other.  In the 20th anniversary model in particular, you
+    raise two digimon at a time, and they can engage in tag team battles
+    or train together. The team training worked as a guessing game,
+    where pressing the A button would cause the creature you're
+    controlling to attack high and the B button would attack low. The
+    other creature would then block high or low, seemingly at random.
+    This would occur 5 times, and if 3 of your 5 attacks hit, the
+    training session would count as a success and improve the combat
+    ability of both creatures.
+
+    While refamiliarizing myself with the functions of the unit, I
+    recalled discovering previously that there were only six different
+    patterns that the system used to determine where the opposing
+    creaturee would block.  I decided to embark on an attempt to find
+    the perfect attack sequence that would defeat all 6 patterns.    '''
+
+# these are the inputs to beat the block patterns
 BATTLE_SEQUENCES = (
     ('A', 'B', 'A', 'B', 'A'),
     ('A', 'B', 'A', 'B', 'B'),
@@ -19,17 +38,58 @@ BATTLE_SEQUENCES = (
     ('B', 'A', 'B', 'A', 'B')
                    )
 
-# generate all possible input sequences
-input_sequences = itertools.product('AB', repeat=6)
+# generator for all possible input sequences
+input_sequences = product('AB', repeat=5)
 
-# function to determine if listed sequence wins
-## testing zip
-example_a = ('A', 'A', 'A', 'A', 'A')
-example_b = ('B', 'B', 'B', 'B', 'B')
-example_c = ('A', 'B', 'A', 'B', 'A')
 
-for comp in zip(example_a, example_c):
-    print(comp[0] == comp[1])
-print('\n')
-for comp in zip(example_b, example_c):
-    print(comp[0] == comp[1])
+def winner(tested, target):
+    ''' compare the two sequences,
+        return true if 3 of 5 match '''
+    win_count = 0
+    for comp in zip(tested, target):
+        if comp[0] == comp[1]:
+            win_count += 1
+    return bool(win_count > 2)
+
+
+def success_count(test_list, target_collection):
+    ''' compare the sequence against the list of
+        sequences and return how many of them win   '''
+    successes = 0
+    for battle in target_collection:
+        if winner(test_list, battle):
+            successes += 1
+        else:
+            continue
+    return successes
+
+
+def main():
+    ''' check every possible sequence to see how many it wins,
+        print if it meets criteria                              '''
+    for attempt in input_sequences:
+        wins = success_count(attempt, BATTLE_SEQUENCES)
+        if wins > 3:
+            print(attempt)
+        else:
+            continue
+
+
+if __name__ == "__main__":
+    main()
+
+
+""" In the end, the experiment was technically a failure.
+
+    I was unable to get a sequence that wins every time.  In hindsight,
+    I suppose that's why the developers programmed 6 of them: to
+    prevent the user from finding a win button and reducing the game
+    to repetition. It was redundant to make 6, as I couldn't find a
+    sequence that wins in 5 of the 6 patterns, either. I did find
+    several that win 4 out of 6 times, so perhaps I'll come back to
+    this to find a sequence that can tell me quickly if I'll need to
+    change the last input or two.
+
+    In the meantime, I've learned to use for the zip() function, and
+    discovered the itertools module, so as a python practice script,
+    this was a rousing success.                                     """
